1cafbf9ae2d6a4f1e399d2c3ed0739ded3655666
Gulay Nazarova <97050713+glynzr@users.noreply.github.com>
2023-04-27 15:40:18 +0400

    Create Task6.c

---
+++ MockExam_1/Task6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    scanf("%i",&n);
+    int* arr= (int*) malloc(n*sizeof(int));
+    int max_=INT_MIN;
+    int min_=INT_MAX;
+    for(int i=0;i<n;i++){
+        scanf("%i",arr+i);
+        if(i==0) {max_=*arr; min_=*arr;}
+        else{
+            if(min_>*(arr+i)) min_=*(arr+i);
+            if(max_<*(arr+i)) max_=*(arr+i);
+        }
+    }
+
+
+    for(int i=0;i<n;i++){
+        if(*(arr+i)==max_) printf("%i ",min_);
+        else printf("%i ",*(arr+i));
+    }
+    free(arr);
+}
