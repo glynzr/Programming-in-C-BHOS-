137dee575f9af4dbc6565b6c4fd106173d2f0af0
Gulay Nazarova <97050713+glynzr@users.noreply.github.com>
2023-04-28 12:16:33 +0400

    Update Task_17.c

--- Lab2/Task_17.c
+++ Lab2/Task_17.c
@@ -18,3 +18,26 @@ int main(){
         printf("No\n");
     }
 }
+
+/*#include <stdio.h>
+int isPalindrome(int x){
+    int arr[100];
+    int i=0;
+    while(x>0){
+        *(arr+i)=x%10;
+        x/=10;
+        i++;
+    }
+
+    for(int j=0;j<i;j++){
+        if(arr[j]!=arr[i-j-1]) return 0;
+    }
+    return 1;
+
+}
+int main(){
+    int n;
+    scanf("%i",&n);
+    printf("%s\n",isPalindrome(n) ? "Palindrome" : "Not palindrome");
+}
+*/
