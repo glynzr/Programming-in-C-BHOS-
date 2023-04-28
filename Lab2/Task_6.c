b621a8f1dfbb735b67618e988fa6dd897bed1f4f
Gulay Nazarova <97050713+glynzr@users.noreply.github.com>
2023-04-28 12:01:30 +0400

    Update Task_6.c

--- Lab2/Task_6.c
+++ Lab2/Task_6.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(){
-    int n;
-    scanf("%i",&n);
-    int sum=0;
-    if(n>0){
-        sum=(1+n)*n/2;
-    }else{
-        sum=(1+n)*(2-n)/2;
-    }
-    printf("%i",sum);
+typedef long long int lli;
+int main(){ 
+    lli n;
+    scanf("%lli",&n);
+    printf("%lli",n>0 ? (1+n)*n/2 : (1+n)*(2-n)/2);
 }
