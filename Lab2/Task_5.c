f258e3b3c280b763639397cfaccc215e3d1a000c
Gulay Nazarova <97050713+glynzr@users.noreply.github.com>
2023-04-28 11:55:48 +0400

    Update Task_5.c

--- Lab2/Task_5.c
+++ Lab2/Task_5.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include <math.h>
-double triangleAngle(double x1,double y1,double x2,double y2,double x3,double y3 ){
-    double a=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-    double b=sqrt((x1-x3)*(x1-x3)+(y1-y3)*(y1-y3));
-    double c=sqrt((x2-x3)*(x2-x3)+(y2-y3)*(y2-y3));
-
-
-    double cosA=(b*b+c*c-a*a)/(2*b*c);
-    double angleA=acos(cosA)*180/M_PI;
-
-    double cosB=(a*a+c*c-b*b)/(2*a*c);
-    double angleB=acos(cosB)*180/M_PI;
-
-    double cosC=(b*b+a*a-c*c)/(2*b*a);
-    double angleC=acos(cosC)*180/M_PI;
-
-    double maxAngle=angleA;
-    if(maxAngle<angleB) maxAngle=angleB;
-    if(maxAngle<angleC) maxAngle=angleC;
-
-    return maxAngle;
+double distance(int a1,int b1,int a2,int b2){
+    return sqrt(pow((a1-a2),2)+pow((b1-b2),2));
 }
-int main(){
-    double  x1,y1,x2,y2,x3,y3;
-    scanf("%lf%lf%lf%lf%lf%lf",&x1,&y1,&x2,&y2,&x3,&y3);
-    printf("%.6lf",triangleAngle(x1,y1,x2,y2,x3,y3));
 
+double angle(double side1,double side2,double side3){
+    return acos((side1*side1+side2*side2-side3*side3)/(2*side1*side2));
+}
+
+int main(){
+    int x1,y1,x2,y2,x3,y3;
+    scanf("%i%i%i%i%i%i",&x1,&y1,&x2,&y2,&x3,&y3);
+    double a=distance(x1,y1,x2,y2);
+    double b=distance(x2,y2,x3,y3);
+    double c=distance(x1,y1,x3,y3);
+
+    double angle1=angle(a,b,c);
+    double angle2=angle(b,c,a);
+    double angle3=angle(c,a,b);
+
+    double max_=angle1;
+    if(max_<angle2) max_=angle2;
+    if(max_<angle3) max_=angle3;
+    printf("%.6lf",max_*180/acos(-1));
 }
